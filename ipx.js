9ba2b7d10a42f252f109074c0d0886fbe0568022
DarkKnight <94906343+l0v3m0n3y@users.noreply.github.com>
2024-08-30 21:39:15 +0300

    Update ipx.js

--- ipx.js
+++ ipx.js
@@ -1,8 +1,7 @@
 class ipx{
     constructor(){
         this.api = "https://ipx.ac"
-        this.headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0","host":"tempimail.org"}
-        this.token=null
+        this.headers={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"}
     }
     async my_ip(){
         let req=await fetch(`${this.api}/geo/`,{method:"GET",headers: this.headers});
